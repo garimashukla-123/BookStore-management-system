5ea3a340a56652b9d0e8a381d8f504754e177ec0
garimashukla-123 <105700713+garimashukla-123@users.noreply.github.com>
2023-12-18 18:45:55 +0530

    Add files via upload

--- go-bookstore/pkg/models/book.go
+++ go-bookstore/pkg/models/book.go
@@ -6,7 +6,8 @@ import(
 
 )
 
-var db *gorm.DBtype Book struct{
+var db *gorm.DB
+type Book struct{
 	gorm.model
 	Name string 'gorm:""json:"name"'
 	Author string 'json:"author"'
@@ -17,4 +18,26 @@ func init(){
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book{
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+func GetBookById(Id int64) (*Book, *gorm.DB){
+	var getBook Book
+	db:=db.Where("ID=?" , Id).Find(&getBook)
+	return &getBook, db
+}
+func DeleteBook(ID int64) Book{
+	var book Book
+	db.Where("ID=?",ID).Delete(book)
+	return book
 }
